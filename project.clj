519b044bafbf9bbdd6256f88cf82a3fa69a35bfb
Nathan Flint <nlflint@gmail.com>
2015-05-29 09:26:31 -0700

    Updated project file to work with light table

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject core.logic-koans "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/core.logic "0.6.5"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/core.logic "0.8.10"]]
   :dev-dependencies [[lein-koan "0.1.1"]])
